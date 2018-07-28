13363f1655619d84b7ade29d3f458064b321db16
Ryan Hendricks <ryanhendricks@gmail.com>
2018-07-27 22:46:23 -0400

    Improved ethengine vuex modules

--- src/store/ethengine/index.js
+++ src/store/ethengine/index.js
@@ -1,10 +1,10 @@
-/* eslint-disable */
 
 import Web3 from 'web3';
 // import utils from 'web3-utils';
 import mutations from './mutations';
 import state from './state';
 // import getters from './getters';
+import { Notify } from 'quasar';
 
 // const BN = web3.utils.BN
 import ZeroClientProvider from 'web3-provider-engine/zero.js';
@@ -16,14 +16,20 @@ import ZeroClientProvider from 'web3-provider-engine/zero.js';
 // rpcUrl: 'https://kovan.infura.io',
 
 const actions = {
-  async initWeb3({ state, commit, dispatch }) {
-    await new Promise((resolve, reject) => {
+  initWeb3({ commit, dispatch }) {
+    try {
+      let web3Provider;
       if (global.web3) {
-        var web3Provider = global.web3.currentProvider;
+        web3Provider = global.web3.currentProvider;
+        commit('IS_INJECTED', true);
+        commit('SET_PROVIDER', 'injected');
       } else {
         commit('SET_HOST', state.fallbackHost);
+        commit('IS_INJECTED', false);
+        commit('SET_PROVIDER', 'infura');
+
         web3Provider = ZeroClientProvider({
-          getAccounts: function() {},
+          getAccounts() {},
           rpcUrl: state.rpcUrl,
         });
       }
@@ -31,85 +37,109 @@ const actions = {
         global.web3 = new Web3(web3Provider);
         dispatch('startChecking');
       } else {
-        reject('error');
+        commit('SET_CONNECTED', false);
       }
-    });
+    } catch (e) {
+      dispatch('createEngineNotify', e);
+    }
   },
   startChecking({ dispatch }) {
     setInterval(() => {
       dispatch('check');
     }, state.updateInterval);
   },
-  async check({ dispatch }) {
-    await dispatch('checkNetwork');
-    await dispatch('getBlockNumber');
-    await dispatch('getGasPrice');
-    await dispatch('checkConnection');
-    await dispatch('checkAccount');
-
+  check({ dispatch }) {
+    dispatch('checkNetwork');
+    dispatch('getBlockNumber');
+    dispatch('getGasPrice');
+    dispatch('checkConnection');
+    dispatch('checkAccount');
   },
-  async checkNetwork({ state, commit, dispatch }) {
+  async checkNetwork({ commit, dispatch }) {
     try {
       const network = await global.web3.eth.net.getId();
-      commit('UPDATE_NETWORK', network);
-    } catch(e) {
-      dispatch('settings/createNotify', e);
+      if (network !== state.netId) {
+        commit('UPDATE_NETWORK', network);
+        commit('SET_CONNECTED', true);
+      }
+    } catch (e) {
+      dispatch('createEngineNotify', 'checknetwork error');
     }
   },
-  async checkAccount({ state, commit, dispatch }) {
+  async checkAccount({ commit, dispatch }) {
     await global.web3.eth.getAccounts((error, accounts) => {
-      if (error) throw new Error(error);
+      if (error) dispatch('createEngineNotify', error);
       if (accounts.length && state.account !== accounts[0]) {
         commit('UPDATE_UNLOCKED', true);
         commit('UPDATE_ACCOUNT', accounts[0]);
         commit('IS_INJECTED', true);
-        dispatch('getBalance', accounts[0]);
-      } else if (!accounts.length) {
+        dispatch('getBalance');
+      } else if (!accounts.length && state.unlocked !== false) {
         commit('UPDATE_UNLOCKED', false);
-        commit('UPDATE_ACCOUNT', "Please Unlock Metamask");
+        commit('UPDATE_ACCOUNT', 'Please Unlock Metamask');
       }
     });
   },
-  async getBalance({ commit, state }) {
-    await new Promise((resolve, reject) => {
-      const coinbase = state.account;
-      if (!coinbase) {
-        return resolve('0');
+  async getBalance({ commit, dispatch }) {
+    try {
+      if (!state.account || state.unlocked === false) {
+        commit('SET_BALANCE', 0);
+      } else {
+        await global.web3.eth.getBalance(state.account)
+          .then((rawBalance) => {
+            const balance = global.web3.utils.fromWei(rawBalance.toString(10), 'ether');
+            commit('SET_BALANCE', balance);
+          });
       }
-      global.web3.eth.getBalance(state.account, (err, result) => {
-        if (err) {
-          reject(err);
-        } else {
-          const balance = global.web3.utils.fromWei(result.toString(10), 'ether');
-          if (state.balance !== balance) commit('SET_BALANCE', balance);
-          resolve(balance);
-        }
-      });
-    });
+    } catch (e) {
+      dispatch('createEngineNotify', e.valueOf());
+    }
   },
-  async getBlockNumber({ state, commit, dispatch }) {
-    await global.web3.eth.getBlockNumber((err, block) => {
-      if (err) throw new Error(err);
-      if (block) {
-        commit('UPDATE_BLOCK', block);
-      }
-    });
+  async getBlockNumber({ commit, dispatch }) {
+    try {
+      await global.web3.eth.getBlockNumber()
+        .then((block) => {
+          if (block && state.blockNumber !== block) {
+            commit('UPDATE_BLOCK', block);
+          }
+        });
+    } catch (e) {
+      dispatch('createEngineNotify', e);
+    }
   },
-  async getGasPrice({ state, commit, dispatch }) {
-    await global.web3.eth.getGasPrice((err, rawprice) => {
-      if (err) throw new Error(err);
-      if (rawprice) {
-        let gasPrice = global.web3.utils.fromWei(rawprice, 'ether').toString(10) * 1000000000;
-        commit('UPDATE_GASPRICE', gasPrice);
-      }
-    });
+  async getGasPrice({ commit, dispatch }) {
+    try {
+      await global.web3.eth.getGasPrice()
+        .then((rawPrice) => {
+          const gasPrice = global.web3.utils.fromWei(rawPrice, 'ether').toString(10) * 1000000000;
+          if (gasPrice && state.gasPrice !== gasPrice) {
+            commit('UPDATE_GASPRICE', gasPrice);
+          }
+        });
+    } catch (e) {
+      dispatch('createEngineNotify', e);
+    }
   },
-  async checkConnection({ state, commit, dispatch }) {
-    await global.web3.currentProvider.isConnected((err, connected) => {
-      if (err) throw new Error(err);
-      if (connected === true) {
-        commit('IS_CONNECTED', true);
-      }
+  async checkConnection({ commit, dispatch }) {
+    try {
+      await global.web3.eth.net.isListening()
+        .then((connected) => {
+          if (connected === true && state.connected !== true) {
+            commit('SET_CONNECTED', true);
+          }
+        });
+    } catch (e) {
+      dispatch('createEngineNotify', e);
+    }
+  },
+
+  createEngineNotify(message) {
+    const msg = message.toString();
+    Notify.create({
+      msg,
+      color: 'primary',
+      textColor: 'white', // if default 'white' doesn't fits
+      icon: 'memory',
     });
   },
 };
--- src/store/ethengine/mutations.js
+++ src/store/ethengine/mutations.js
@@ -1,32 +1,25 @@
 export default {
   UPDATE_NETWORK(state, network) {
-    if (state.network === network) {
-      return;
-    }
+    state.netId = network;
     switch (network) {
       case 1:
         state.network = ' Main Net';
-        state.provider = 'Infura';
         state.rpcURL = 'https://mainnet.infura.io';
         break;
       case 2:
         state.network = ' Morden Testnet (deprecated)';
-        state.provider = 'Infura';
         state.rpcURL = 'https://mainnet.infura.io';
         break;
       case 3:
         state.network = ' Ropsten Testnet';
-        state.provider = 'Infura';
         state.rpcURL = 'https://ropsten.infura.io';
         break;
       case 4:
         state.network = ' Rinkeby Testnet';
-        state.provider = 'Infura';
         state.rpcURL = 'https://rinkeby.infura.io';
         break;
       default:
         state.network = 'local';
-        state.provider = 'local';
         state.rpcURL = 'https://127.0.0.1:8545';
         break;
     }
@@ -49,10 +42,13 @@ export default {
   SET_BALANCE(state, balance) {
     state.balance = balance;
   },
-  IS_CONNECTED(state, val) {
+  SET_CONNECTED(state, val) {
     state.connected = val;
   },
   IS_INJECTED(state, injected) {
     state.isInjected = injected;
+  },
+  SET_PROVIDER(state, provider) {
+    state.provider = provider;
   },
 };
